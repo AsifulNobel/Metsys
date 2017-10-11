0f2711ad0fb98147720ed2aa02e30eab906ac19d
Asiful Nobel <asif.nobel@gmail.com>
2017-10-11 13:00:19 +0600

    Added little CSS to moderator pages

--- chatbot/forms.py
+++ chatbot/forms.py
@@ -3,17 +3,17 @@ from .models import (ClassTag, BanglaRequests, EnglishRequests,
 BanglaResponses, EnglishResponses)
 
 class LoginForm(forms.Form):
-    userName = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    userName = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 class EnglishTagForm(forms.Form):
     tag_choices = [(x.tagName, x.tagName.upper()) for x in ClassTag.objects.raw('SELECT distinct c.id, c."tagName" from chatbot_classtag as c inner join chatbot_englishrequests as e on c.id=e.tag_id order by c."tagName" asc;')]
-    tag = forms.CharField(widget=forms.Select(choices=tag_choices))
+    tag = forms.CharField(widget=forms.Select(choices=tag_choices, attrs={'class':'form-control'}))
 
 class BanglaTagForm(forms.Form):
     tag_choices = [(x.tagName, x.tagName.upper()) for x in ClassTag.objects.raw('SELECT distinct c.id, c."tagName" from chatbot_classtag as c inner join chatbot_banglarequests as e on c.id=e.tag_id order by c."tagName" asc;')]
-    tag = forms.CharField(widget=forms.Select(choices=tag_choices))
+    tag = forms.CharField(widget=forms.Select(choices=tag_choices, attrs={'class':'form-control'}))
 
 class NewTagForm(forms.Form):
-    new_tag = forms.CharField()
-    response = forms.CharField()
+    new_tag = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    response = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
--- metsys/urls.py
+++ metsys/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import home
 
+app_name = 'metsys'
+
 urlpatterns = [
 	url(r'^$', home, name='home'),
 ]
