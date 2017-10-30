b3d06b5b2abd253df3c47b120769c371a9b89b1f
Asiful Nobel <asif.nobel@gmail.com>
2017-10-30 23:40:46 +0600

    Update: Registered New Database Model in Admin

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (BanglaRequests, BanglaResponses, EnglishRequests, EnglishResponses, ClassTag, Complaints, Feedbacks)
+from .models import (Agent, BanglaRequests, BanglaResponses, EnglishRequests, EnglishResponses, ClassTag, Complaints, Feedbacks)
 
 # Register your models here.
-admin.site.register([BanglaRequests, BanglaResponses, EnglishRequests, EnglishResponses, ClassTag, Complaints, Feedbacks])
+admin.site.register([Agent, BanglaRequests, BanglaResponses, EnglishRequests, EnglishResponses, ClassTag, Complaints, Feedbacks])
